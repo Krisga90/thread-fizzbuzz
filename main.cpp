c99beba0462a1714db4a7039e02834ab4f0d9678
Krisga <krzysiekgdynia@poczta.fm>
2023-07-24 22:01:35 +0200

    first commit

---
+++ main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <string>
+#include <thread>
+#include <vector>
+
+using namespace std;
+
+void fizBuz(const vector<string> &names);
+// void fizBuz(const vector<string> &names, int times); will not work, there is no overload of functions
+
+int main() {
+  vector<string> names{"Abdul", "Brat", "Claudia", "Dave"};
+  thread tr(fizBuz, std::ref(names));
+  tr.join();
+
+  return 0;
+}
+
+
+
+void fizBuz(const vector<string> &names) {
+  for (int num = 1; num <= 30; num++) {
+    cout << names[(num -1) % 4] << " say ";
+
+    if (num % 3 == 0 && num % 5 == 0) {
+      cout << "fizzbuzz!";
+    } else if (num % 3 == 0) {
+      cout << "fizz!";
+    } else if (num % 5 == 0) {
+      cout << "buzz!";
+    } else {
+      cout << num;
+    }
+    std::cout << "\n";
+  }
+}
+
+// void fizBuz(const vector<string> &names, int times)
+// {
+//   for (int num = 1; num <= times; num++) {
+//     cout << names[(num -1) % 4] << " say ";
+
+//     if (num % 3 == 0 && num % 5 == 0) {
+//       cout << "fizzbuzz!";
+//     } else if (num % 3 == 0) {
+//       cout << "fizz!";
+//     } else if (num % 5 == 0) {
+//       cout << "buzz!";
+//     } else {
+//       cout << num;
+//     }
+//     std::cout << "\n";
+//   }
+// }
